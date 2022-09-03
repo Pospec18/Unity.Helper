1d26884a83ea9ac7ecd4c5333ac6706b828cf6e0
Jan Pospisil <jenda.pospec@gmail.com>
2022-09-03 15:32:48 +0200

    Dethable abstract class

--- Runtime/Hit Detection/Hitable.cs
+++ Runtime/Hit Detection/Hitable.cs
@@ -9,4 +9,55 @@ namespace Pospec.Helper.Hit
     {
         public void TakeHit(float damage, Transform attacker);
     }
+
+    public abstract class Deathable : MonoBehaviour, IHitable
+    {
+        [SerializeField, Min(1)] protected float maxHealth = 4;
+
+        protected float _health;
+        protected float Health
+        {
+            get => _health;
+            set
+            {
+                _health = value;
+                if(_health > maxHealth)
+                    _health = maxHealth;
+
+                if(_health <= 0)
+                    Death();
+            }
+        }
+
+        protected virtual void Start()
+        {
+            FullyHeal();
+        }
+
+        public void FullyHeal()
+        {
+            Health = maxHealth;
+            OnFullHeal();
+        }
+
+        protected virtual void OnFullHeal() { }
+
+        public void HealBy(float value)
+        {
+            Health += value;
+            OnHealBy(value);
+        }
+
+        protected virtual void OnHealBy(float value) { }
+
+        public void TakeHit(float damage, Transform attacker)
+        {
+            Health -= damage;
+            OnTakeHit(damage, attacker);
+        }
+
+        protected virtual void OnTakeHit(float damage, Transform attacker) { }
+
+        public abstract void Death();
+    }
 }
